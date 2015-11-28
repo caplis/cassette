4476de1574026b3a4dec350bc9bb8b09c179666d
Tim Caplis <caplis@gmail.com>
2015-11-27 16:26:13 -0800

    fix collection.at, just return undefined no need to check bounds

--- test/acceptance/model.js
+++ test/acceptance/model.js
@@ -4,82 +4,67 @@ let cass = require('cassandra-driver');
 let client = new cass.Client({
     contactPoints: ['localhost'],
     authProvider: new cass.auth.PlainTextAuthProvider(
-        process.env.PLAT_CASSANDRA_USERNAME,
-        process.env.PLAT_CASSANDRA_PASSWORD
+        'cassandra',
+        'cassandra'
     )
 });
 let Cassette = require('../../index');
 let cassette = new Cassette(client);
 let joi = require('joi');
-let post_def = {
-    user_id: joi.string(),
-    post_id: joi.string(),
-    subject: joi.string(),
-    body: joi.string().allow(''),
-    image_id: joi.string(),
-    repost_user_id: joi.string(),
-    repost_id: joi.string(),
-    repost_body: joi.boolean(),
-    product_user_id: joi.string(),
-    product_id: joi.string(),
-    product_currency: joi.string(),
-    product_price: joi.number(),
-    product_quantity: joi.number(),
-    product_link: joi.string().allow(''),
-    product_status: joi.string(),
+let user_def = {
+    user_id: joi.string().regex(/\d+/),
+    name: joi.string().min(3),
     created_at: joi.date(),
     updated_at: joi.date(),
-    primary_key: ['user_id','post_id'],
-    default_order: 'DESC'
+    primary_key: ['user_id']
 };
-let posts = cassette.define({
-    keyspace:'post',
-    table:'post',
-    definition: post_def,
-    client: client
+let users = cassette.define({
+    keyspace:'test',
+    table:'user',
+    definition: user_def
 });
 
-let params = {user_id:'22469097056894976', post_id:'22'};
-let post = posts.create(params);
-console.log(post);
+let params = {user_id:'22469097056894976'};
+let user = users.create(params);
+console.log(user);
 console.log('FIRST SAVE >>');
-post.save(function (err) {
+user.save(function (err) {
     if (err) {
         console.log(err);
         process.exit();
     }
-    console.log(post.to_object());
+    console.log(user.to_object());
 
-    // updat model
-    post.subject = 'test';
-    post.body = 'rock you like a hurricane';
+    // update model
+    user.name = 'John Smith';
 
     console.log('SECOND SAVE >>');
-    post.save(function (err) {
+    user.save(function (err) {
         if (err) {
             console.log(err);
             process.exit();
         }
-        console.log(post.to_object());
+        console.log(user.to_object());
+        user.name = 'Jane Smith';
 
         console.log('THIRD SAVE >>');
-        post.save(function (err) {
+        user.save(function (err) {
             if (err) {
                 console.log(err);
                 process.exit();
             }
-            console.log(post.to_object());
+            console.log(user.to_object());
 
             console.log('SYNC >>');
-            post.sync(function (err) {
+            user.sync(function (err) {
                 if (err) {
                     console.log(err);
                     process.exit();
                 }
-                post.body = 'this is a test';
-                console.log(post);
-                console.log(post.to_object());
-                post.delete(function(err) {
+                user.name = 'John Q. Voter';
+                console.log(user);
+                console.log(user.to_object());
+                user.delete(function(err) {
                     if (err) {
                         console.log(err);
                         process.exit();
